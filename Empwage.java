9a6e63b732f52ac9ae1e9233b77b568dce5b56e9
ishuks9 <ishuks9@gmail.com>
2021-06-29 08:51:55 +0530

    inintialised

---
+++ Empwage.java
@@ -0,0 +1,24 @@
+public class Empwage
+{
+
+  public static void main(String args[])
+{
+
+ 
+
+int fulltime=1;
+
+double empCheck= Math.floor(Math.random()*10)%2;
+
+if(empCheck == fulltime)
+
+System.out.println("Employee is Present");
+
+else
+
+System.out.println("Employee is absent");
+
+}
+
+}
+
