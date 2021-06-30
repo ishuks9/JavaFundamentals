b383d96f25acfed89d696ae4c06e3eaacc38a057
ishuks9 <ishuks9@gmail.com>
2021-06-30 15:27:15 +0530

    updated the third use case

---
+++ LineCompUC3.java
@@ -0,0 +1,51 @@
+public class LineCompUC3
+
+
+{
+
+
+public static void main(String arg[])
+
+
+{
+
+
+
+
+String s1 = new String("Length of first line");
+
+
+String s2 = new String("Length of second line");
+
+
+
+ //result = s1.equals(s2);
+
+
+
+
+//System.out.println("Comparing two Strings using equals method: " + result);
+
+
+int result = s1.compareTo(s2);
+
+
+System.out.println("Comparing two different Objects with compareTo method: " + result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
