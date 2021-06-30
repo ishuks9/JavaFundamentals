9727a262b3e832795c509baf923c69cc0a9a4555
ishuks9 <ishuks9@gmail.com>
2021-06-30 15:15:10 +0530

    pushed the second use case

---
+++ LineCompUC2.java
@@ -0,0 +1,37 @@
+public class LineCompUC2
+
+{
+
+public static void main(String arg[])
+
+{
+
+
+
+String s1 = new String("Length of first line");
+
+String s2 = new String("Length of second line");
+
+
+boolean 
+result = s1.equals(s2);
+
+
+
+System.out.println("Comparing two Strings using equals method: " + result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
